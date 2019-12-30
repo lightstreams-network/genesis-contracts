65e4e96c399e4e9f557cc269aa524a8b6b4c35a8
Michael Smolenski <mike_smo@yahoo.com>
2019-12-30 19:54:56 +0100

    missing file

---
+++ test/generateFans.js
@@ -0,0 +1,49 @@
+require('dotenv').config({ path: `${process.env.PWD}/.env` });
+const { daysInMonth } = require('./utils');
+
+function generateFans(fans, amount, month, days, isSubscriber) {
+  let currentNumOfFans = fans.length;
+  for (let subscriberIndex = currentNumOfFans; subscriberIndex < currentNumOfFans + amount; subscriberIndex++) {
+    fans[subscriberIndex] = {
+        id: subscriberIndex,
+        subscriber: isSubscriber,
+        isSubscribed: false,
+        month: month,
+        buyDay: Math.floor(Math.random() * days) + 1,
+        tokens: 0
+      };
+  }
+
+  return fans;
+}
+
+module.exports.generateFans = ()=>{
+    let fans = [];
+
+    let year = 2020;
+    let dayCount = 0;
+    let growth = 0.10;
+
+    let numOfNew = 3;
+    for (let month = 1; month <= 12; month ++) {
+        let days = daysInMonth(month, year);
+        fans = generateFans(fans, numOfNew, month, days, true);
+        
+        numOfNew = Math.ceil(fans.length*growth);
+    }
+
+    /*
+    numOfNew = 5;
+    growth = 0.5;
+    for (let month = 1; month <= 3; month ++) {
+        let days = daysInMonth(month, year);
+        fans = generateFans(fans, numOfNew, dayCount, days, false);
+        
+        dayCount += days;
+        numOfNew = Math.round(fans.length*growth);
+    }
+    */
+
+    return fans.sort((a, b) => (a.month * 100 + a.buyDay) -  (b.month * 100 + b.buyDay));;
+};
+
